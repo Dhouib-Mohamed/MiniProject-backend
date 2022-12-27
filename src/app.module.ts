0baebb8d8b6eec29cdd21227cc75e24526b54105
dhouib <mohamed.dhouib@insat.ucar.tn>
2022-12-27 21:23:59 +0100

    adding database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from "@nestjs/mongoose";
+import { UserSchema } from "./schema/user.schema";
+import { UserService } from './service/user/user.service';
+import { UserController } from './controller/user/user.controller';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017',{dbName: 'studentdb'}),
+    MongooseModule.forRoot('mongodb://<username>:<password>@localhost:27017',{dbName: 'studentdb'}),
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }])
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
 })
 export class AppModule {}
